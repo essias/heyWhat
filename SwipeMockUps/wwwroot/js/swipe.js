8fdd35e6e04b8503e37bd3b4599d7a71f896456d
jafa <essias@gmail.com>
2020-04-06 12:51:32 -0500

    Add project files.

---
+++ SwipeMockUps/wwwroot/js/swipe.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function dragstart_handler(ev) {
+    // Create an image and then use it for the drag image.
+    // NOTE: change "example.gif" to a real image URL or the image 
+    // will not be created and the default drag image will be used.
+    
+    let img = new Image();
+    img.src = 'h2.jpg';
+    ev.dataTransfer.setDragImage(img, 10, 10);
+    
+}
